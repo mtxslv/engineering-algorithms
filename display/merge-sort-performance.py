98d62160879b9a7f9fcf9892d1d4734199c9634a
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-10-19 14:13:31 -0300

    feat: plot nln(n) line

--- display/merge-sort-performance.py
+++ display/merge-sort-performance.py
@@ -10,12 +10,14 @@ with open('./results/merge-sort/result.json', 'r') as file:
 n = data['n']
 times = data['time']
 
+constante = 107
 y = [
-    i*math.log(i) for i in n
+    (constante*i)*math.log(constante*i) for i in n
 ]
 # Create the plot
 plt.figure(figsize=(10, 6))
 
+plt.plot(n,times,linestyle='-', linewidth=2)
 plt.plot(n,y,linestyle='-', linewidth=2)
-
+plt.grid(True)
 plt.show()
