4eb7a8b75e9d73a8e424a88391bf2874ecd62e40
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-10-22 20:57:55 -0300

    chore: delete moved files

--- matrix-multiplication/matrix-mult.py
+++
@@ -1,5 +0,0 @@
-def regular_matrix_mult(matrix_a, matrix_b, matrix_c, n):
-    for i in range(0, n):
-        for j in range(0, n):
-            for k in range(0,n):
-                matrix_c[i][j] = matrix_c[i][j] + matrix_a[i][k]*matrix_b[k][j] 
