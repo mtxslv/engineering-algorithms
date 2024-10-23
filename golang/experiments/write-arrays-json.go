00cd7ccdeb6fd44acdd7fc0bc6a39bf0a47b8f0e
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-10-23 15:18:27 -0300

    fix: write to json

--- golang/experiments/write-arrays-json.go
+++ golang/experiments/write-arrays-json.go
@@ -3,28 +3,27 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	// "math/rand/v2"
 	"os"
 )
 
 type Dados struct {
-	n []int
-	y []int
+	N []int `json:"n"` // Capitalized field name for export
+	Y []int `json:"y"` // Capitalized field name for export
 }
 
 func generate_array(n int, reversed bool) []int {
 	arr := make([]int, n)
-	for i := 0 ; i < n; i++ {
+	for i := 0; i < n; i++ {
 		if reversed {
 			arr[i] = n - i - 1
 		} else {
 			arr[i] = i
 		}
-	} 
+	}
 	return arr
 }
 
-func check(e error){
+func check(e error) {
 	if e != nil {
 		panic(e)
 	}
@@ -39,8 +38,8 @@ func write_array_json(arr_n, arr_y []int) {
 	check(err)
 }
 
-func main(){
-	array_n := []int{1,2,3}
-	array_y := []int{1,2,3}
+func main() {
+	array_n := []int{1, 2, 3}
+	array_y := []int{1, 2, 3}
 	write_array_json(array_n, array_y)
 }
