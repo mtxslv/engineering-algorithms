b4a0bb24124b00e8c9576bccb5988ca0e0967fd4
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-10-27 20:34:35 -0300

    chore: some changes

--- golang/select/randomized-select.go
+++ golang/select/randomized-select.go
@@ -2,61 +2,51 @@ package main
 
 import (
 	"fmt"
-	"math/rand/v2"
+	"math/rand"
+	"time"
 )
-
 
 func partition(A []int, p int, r int) int {
 	x := A[r] // the pivot
 	i := p - 1 // highest index into the low side
-	for j := p ; j < r ; j++ {
+	for j := p; j < r; j++ {
 		if A[j] <= x {
 			i = i + 1
-			aux := A[i]
-			A[i] = A[j]
-			A[j] = aux
+			A[i], A[j] = A[j], A[i]
 		}
-	} 
-	aux := A[r]
-	A[r] = A[i+1]
-	A[i+1] = aux
+	}
+	A[i+1], A[r] = A[r], A[i+1]
 	return i + 1
 }
 
 func randomizedPartition(A []int, p int, r int, randomizer *rand.Rand) int {
-	i := int32(p) + randomizer.Int32N(int32(r-p))
-	aux := A[i]
-	A[i] = A[r]
-	A[r] = aux
-	return partition(A,p,r)
+	i := p + randomizer.Intn(r-p+1)
+	A[i], A[r] = A[r], A[i]
+	return partition(A, p, r)
 }
 
 func randomizedSelect(A []int, p, r, i int, randomizer *rand.Rand) int {
 	// 1 <= i <= r-p+1 when p==r means that i == 1
 	if p == r {
-		return A[p] 
+		return A[p]
 	}
-	q := randomizedPartition(A,p,r, randomizer)
+	q := randomizedPartition(A, p, r, randomizer)
 	k := q - p + 1
 	if i == k {
 		return A[q] // the pivot value is the answer
-	}else{
-		if i < k {
-			return randomizedSelect(A,p,q-1,i, randomizer)
-		} else {
-			return randomizedSelect(A,q+1,r,i-k, randomizer)
-		}
+	} else if i < k {
+		return randomizedSelect(A, p, q-1, i, randomizer)
+	} else {
+		return randomizedSelect(A, q+1, r, i-k, randomizer)
 	}
 }
 
 func main() {
-	randomizer := rand.New(rand.NewPCG(255, 16515616))
-	A := []int{
-		6,19,4,12,14,9,15,7,8,11,3,13,2,5,10,
-	}
-	p := 0 // first element
-	r := 14 // last position
-	i := 5
-	selection := randomizedSelect(A,p,r,i,randomizer)
-	fmt.Printf("The value is: %d \n(Must be 6)", selection)
+	randomizer := rand.New(rand.NewSource(time.Now().UnixNano()))
+	A := []int{6, 19, 4, 12, 14, 9, 15, 7, 8, 11, 3, 13, 2, 5, 10}
+	p := 0              // first element
+	r := len(A) - 1     // last index of A
+	i := 5              // we want the 5th smallest element
+	selection := randomizedSelect(A, p, r, i, randomizer)
+	fmt.Printf("The value is: %d \n(Must be 6)\n", selection)
 }
