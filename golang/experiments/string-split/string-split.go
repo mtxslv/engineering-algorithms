c2c6cf5888ea1968c7956cea155c1454e25067d4
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-11-02 15:30:53 -0300

    feat: process multi line string

--- golang/experiments/string-split/string-split.go
+++ golang/experiments/string-split/string-split.go
@@ -12,4 +12,20 @@ func main(){
 	for i, word := range words {
 		fmt.Printf("%d-th Word: %s (%d characters)\n",i, word, len(word))
 	}
+	fmt.Println("--------------------------------------")
+	// Now multi line string
+	var stanza string = `Me dá um tempo pra pensar
+						 Tenho que criar coragem pra me arriscar
+						 Por Favor entenda o que eu 'to passando
+						 Só 'to pedindo um tempo, não 'to recusando`
+	filteredStanza := strings.Replace(stanza, "\t","",-1)
+	verses := strings.Split(filteredStanza, "\n")
+	fmt.Printf("%q\n\n",verses)
+	var otherWords []string
+	for i, verse := range verses {
+		fmt.Printf("Processing verse #%d\n", i)
+		otherWords = append(otherWords, strings.Split(verse, " ")...)
+	}
+	fmt.Printf("%q\n\n",otherWords)
+
 }
