efa66541f1ff3dd74a13f12ca32bb7ecee2b83e5
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-11-09 19:41:02 -0300

    feat: time function execution

--- golang/fruit-list/main.go
+++ golang/fruit-list/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	// "fmt"
 	"fmt"
+	"time"
 	"utils/utils"
 )
 
 func main(){
+	// Timing variables
+	var totalInsertionTimeHash int64
+	var insertionTimeHash time.Time
+	var totalInsertionTimeOpenAddressing int64
+	var insertionTimeOpen time.Time	
+
 	// Create a new hash table
 	hashTable := utils.NewHashTable()
 	openAddressing := &utils.HashTableOpenAddressingDoubleHashing{}
@@ -17,20 +24,35 @@ func main(){
 	// Insert fruits and Prices into a regular Hash Table
 	for _, item := range(fruitsAndPrices){
 		name, price := item.GetName(), item.GetPrice()
+
+		insertionTimeHash = time.Now()
 		hashTable.Insert(name, price)
+		totalInsertionTimeHash += time.Since(insertionTimeHash).Nanoseconds()
 	} 
 	
-	// Display Hash Table
-	hashTable.Display()
-
-	fmt.Printf("\n\n")
-
+	
 	// Insert Fruits and Prices into Open Addressing With Double Hashing
 	for _, item := range(fruitsAndPrices) {
 		name, price := item.GetName(), item.GetPrice()
-		openAddressing.HashInsert(name, price)
+		insertionTimeOpen = time.Now()
+		_, worked := openAddressing.HashInsert(name, price)
+		if worked{
+			totalInsertionTimeOpenAddressing += time.Since(insertionTimeOpen).Nanoseconds()
+		} else {
+			fmt.Printf("%s not inserted\n",name)
+		}
 	}
+	
+	// Report Insertion Time
+	fmt.Printf("Took %d nanoseconds to insert in Hash Table\n", totalInsertionTimeHash)
+	fmt.Printf("Took %d nanoseconds to insert with Open Addressing\n", totalInsertionTimeOpenAddressing)
+	
+	
+	// Display Hash Table
+	// hashTable.Display()
 
-	openAddressing.Display()
+	fmt.Printf("\n\n")
+	// Display Open Addressing 
+	// openAddressing.Display()
 
 }
