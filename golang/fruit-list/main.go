a0042280f3a1a64563f7d9efe0a15b8c74b16ffb
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-11-09 18:55:56 -0300

    feat: add open addressing to main

--- golang/fruit-list/main.go
+++ golang/fruit-list/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	// "fmt"
+	"fmt"
 	"utils/utils"
 )
 
 func main(){
 	// Create a new hash table
 	hashTable := utils.NewHashTable()
+	openAddressing := &utils.HashTableOpenAddressingDoubleHashing{}
 
 	// Load fruits and prices
 	fruitsAndPrices := utils.LoadCsv("./fruits-prices.csv")
@@ -18,6 +20,17 @@ func main(){
 		hashTable.Insert(name, price)
 	} 
 	
+	// Display Hash Table
 	hashTable.Display()
+
+	fmt.Printf("\n\n")
+
+	// Insert Fruits and Prices into Open Addressing With Double Hashing
+	for _, item := range(fruitsAndPrices) {
+		name, price := item.GetName(), item.GetPrice()
+		openAddressing.HashInsert(name, price)
+	}
+
+	openAddressing.Display()
 
 }
