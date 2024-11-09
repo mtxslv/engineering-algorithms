f47f1b2343d70ddb25c9257d1139e9471c3bf674
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-11-09 18:34:32 -0300

    feat: load csv into hashtable

--- golang/fruit-list/main.go
+++ golang/fruit-list/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"utils/utils"
 )
 
 func main(){
-	// load fruits and prices
+	// Create a new hash table
+	hashTable := utils.NewHashTable()
+
+	// Load fruits and prices
 	fruitsAndPrices := utils.LoadCsv("./fruits-prices.csv")
+	
+	// Insert fruits and Prices into a regular Hash Table
 	for _, item := range(fruitsAndPrices){
-		fmt.Printf("%+v\n", item)
+		name, price := item.GetName(), item.GetPrice()
+		hashTable.Insert(name, price)
 	} 
+	
+	hashTable.Display()
 
 }
