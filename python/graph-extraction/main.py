3c29c3070c141278ee011a83bfb27de54106e4d0
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-11-21 22:29:49 -0300

    feat: extracy recife's graph using central point

--- python/graph-extraction/main.py
+++ python/graph-extraction/main.py
@@ -1,10 +1,9 @@
 import osmnx as ox
 
-# Define the bounding box for Praça da Comunidade Luso Brasileira
-north, south, east, west = -8.0605, -8.0625, -34.8728, -34.8748
-
-# Download and extract the street graph for the defined area
-graph = ox.graph_from_bbox(north=north, south=south, east=east, west=west, network_type="drive")
+distance = 1550
+city_address = 'Recife, Pernambuco'
+pt = (-8.056556,-34.8731439)
+graph = ox.graph_from_point(pt, dist=distance)
 
 # Plot the graph
 ox.plot_graph(graph)
