90693d557fdd0f9c25c66f9b72704374d2f0bed2
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-11-23 17:51:37 -0300

    feat: draft dijkstra algorithm

--- python/graph-extraction/dijkstra-algorithm.py
+++ python/graph-extraction/dijkstra-algorithm.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from pathlib import Path
 
 import numpy as np
@@ -11,6 +12,14 @@ def initialize_single_source(G, source_node_id):
     G.nodes[source_node_id]['d'] = 0
     return G
 
+def dijkstra(G, w, source_node_id):
+    G = initialize_single_source(G,source_node_id)
+    S = set()
+    Q = deepcopy(G.nodes)
+    while len(Q) > 0:
+        pass
+    return G # change later
+
 def load_graph():
     file_path = Path().home() / "Documentos/engineering-algorithms/python/graph-extraction/recife_praca_comunidade.graphml"
     return ox.load_graphml(str(file_path))
@@ -19,7 +28,7 @@ def main():
     SOURCE_NODE_ID = 3691433990
     TARGET_NODE_ID = 3921998309
     G = load_graph()
-    # G = dijkstra(G,0,TARGET_NODE_ID)
+    G = dijkstra(G,0,TARGET_NODE_ID)
     print(G.nodes[TARGET_NODE_ID])
     # print(G.out_edges(SOURCE_NODE_ID,data=True))
 
