7ad36cc0fce84fe3d8eb120b8dac3e130dbf4035
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-11-27 21:13:34 -0300

    refactor: modifications in run.sh file
    - run serial 3 times
    - generate folder structure

--- cpp/experiment-matrix-multiplication/run.sh
+++ cpp/experiment-matrix-multiplication/run.sh
@@ -2,11 +2,27 @@
 
 SEEDA=1223334444
 SEEDB=1224444333
-N=2048
+N=4
+RESULTSFOLDER=./results
 
+if [ ! -d "$RESULTSFOLDER" ]; then 
+    echo "$RESULTSFOLDER does not exist. Making it..."
+    mkdir $RESULTSFOLDER
+    mkdir $RESULTSFOLDER/parallel
+    mkdir $RESULTSFOLDER/serial
+fi
+
+# COMPILE CODE
 g++ -o generate generate.cpp
 g++ -o matmult matmultserial.cpp
 
+# GENERATE MATRICES
 ./generate $N $SEEDA ./matrix_a.txt
 ./generate $N $SEEDB ./matrix_b.txt
-./matmult ./matrix_a.txt ./matrix_b.txt ./ans.txt
+
+# RUN SERIAL PART
+for i in {1..3}; do
+    ./matmult ./matrix_a.txt ./matrix_b.txt $RESULTSFOLDER/serial/ans_$i.txt
+done
+
+#https://stackoverflow.com/questions/59838/how-do-i-check-if-a-directory-exists-or-not-in-a-bash-shell-script
