bf2719c066ffde700d123ed8a4d1cab55518ea02
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-11-28 22:09:19 -0300

    feat: run several times for diff threads

--- cpp/experiment-matrix-multiplication/run.sh
+++ cpp/experiment-matrix-multiplication/run.sh
@@ -4,11 +4,7 @@ SEEDA=1223334444
 SEEDB=1224444333
 N=2048
 RESULTSFOLDER=./results
-NUM_THREADS=10
-
-export OMP_NUM_THREADS=$NUM_THREADS
-
-echo "Using $NUM_THREADS threads."
+HOWMANYTHREADS='5 10 15'
 
 if [ ! -d "$RESULTSFOLDER" ]; then 
     echo "$RESULTSFOLDER does not exist. Making it..."
@@ -32,8 +28,18 @@ for i in {1..3}; do
 done
 
 # RUN PARALLEL PART
-for i in {1..3}; do
-    ./matmultparallel ./matrix_a.txt ./matrix_b.txt $RESULTSFOLDER/parallel/ans_$i.txt $RESULTSFOLDER/parallel/log_$i.txt
+for numThread in $HOWMANYTHREADS; do
+    export OMP_NUM_THREADS=$numThread
+    echo "Using $numThread threads."
+    resultsFolderPath=$RESULTSFOLDER/parallel/$numThread
+    if [ ! -d "$resultsFolderPath" ]; then 
+        echo "$resultsFolderPath does not exist. Making it..."
+        mkdir $resultsFolderPath
+    fi
+    for i in {1..3}; do
+        ./matmultparallel ./matrix_a.txt ./matrix_b.txt $resultsFolderPath/ans_$i.txt $resultsFolderPath/log_$i.txt
+    done
 done
+
 
 #https://stackoverflow.com/questions/59838/how-do-i-check-if-a-directory-exists-or-not-in-a-bash-shell-script
