3633bbf2a0241f896c93d94d58cc0821b9ede2d3
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-12-05 21:48:35 -0300

    feat: define a double linked list

--- golang/move-to-front/utils/linkedlist.go
+++ golang/move-to-front/utils/linkedlist.go
@@ -2,3 +2,44 @@ package utils
 
 // import "fmt"
 
+type Node struct{
+	Previous *Node
+	Content *Item
+	Next *Node
+}
+
+type LinkedList struct {
+	Head *Node
+	Tail *Node
+	len int
+}
+
+func New() *LinkedList {
+	return &LinkedList{}
+}
+
+// Add appends an Item to the end of the linked list
+func (ll *LinkedList) Add(item *Item) {
+	// Create a new node with the given item.
+	newNode := &Node{
+		Content: item,
+		Next: nil, // it will be the last node, so no next node
+	}
+
+	if ll.len == 0 {
+		// List empty. Then newNode is both Head and Tail
+		ll.Head = newNode
+		ll.Tail = newNode
+
+	} else {
+		// List is not empty, append the node at the end
+
+		// newNode's previous node is current Tail
+		newNode.Previous = ll.Tail 
+		// current Tail's next node is newNode
+		ll.Tail.Next = newNode 
+		// newNode is the new Tails
+		ll.Tail = newNode
+	}
+	ll.len++
+}
