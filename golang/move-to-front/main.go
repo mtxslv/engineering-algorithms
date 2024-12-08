c3d4b715d6f842df71fbb6d2a69fd96a9fd97bf4
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-12-08 14:33:31 -0300

    feat: take a sample of song names and run the search & move w/ cost

--- golang/move-to-front/main.go
+++ golang/move-to-front/main.go
@@ -4,7 +4,21 @@ import (
 	"fmt"
 	"os"
 	"utils/utils"
+	"math/rand"
+    "time"
 )
+
+func getSampleSongNames(tracklist []utils.Item, sampleSize int) []string {
+	names := make([]string, sampleSize)
+	randomizer := rand.New(rand.NewSource(time.Now().UnixNano()))
+	i := 0
+	for i < sampleSize {
+		position := randomizer.Intn(len((tracklist)))
+		names[i] = tracklist[position].Title
+		i++
+	}
+	return names
+}
 
 func main() {
 
@@ -25,34 +39,32 @@ func main() {
 	ll := utils.New()
 
 	// Add items to the linked list
-	for it, track := range trackList {
+	for _, track := range trackList {
 		ll.Add(&track)
-		if it == 5 {
-			break
-		}
 	} 
 
-	fmt.Printf("\n\n")
-	// Traverse and print the linked list.
-	current := ll.Head
-	for current != nil {
-		fmt.Printf("%+v\n", *current.Content)
-		current = current.Next
-	}	
-	
-	song := ll.SearchAndMoveToFront("I Wanna Be Yours")
-	
-	if song != nil {
-		fmt.Printf("\n\t The music you looked for: %+v\n", *song)
-	} else {
-		fmt.Printf("\n\t Couldn't find your music :(\n")
-	}
+	// Randomly sample 50 song names to look for 
+	names := getSampleSongNames(trackList, 50)
+
 
 	fmt.Printf("\n\n")
-	// Traverse and print the linked list.
-	current = ll.Head
-	for current != nil {
-		fmt.Printf("%+v\n", *current.Content)
-		current = current.Next
-	}		
+
+	for _, songName := range names {
+		songMetadata, cost := ll.SearchAndMoveToFrontWithCostIncurred(songName)
+		if songMetadata != nil {
+			fmt.Printf("\n\t The music you looked for: %+v\n (took %d operations)", *songMetadata, cost)
+		} else {
+			fmt.Printf("\n\t Couldn't find your music :(\n [Took %d operations]", cost)
+		}
+	
+	}
+	// song, cost := ll.SearchAndMoveToFrontWithCostIncurred("I Wanna Be Yours")
+	
+
+	// // Traverse and print the linked list.
+	// current := ll.Head
+	// for current != nil {
+	// 	fmt.Printf("%+v\n", *current.Content)
+	// 	current = current.Next
+	// }	
 }
