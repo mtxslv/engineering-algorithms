80a4d7ee2019103355430fa2a379d34efffb313b
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-12-13 18:00:36 -0300

    feat: new plot

--- python/move-to-front/main.py
+++ python/move-to-front/main.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import re
-
+import numpy as np
 from matplotlib import pyplot as plt
 
 data_path = Path(__file__).parents[2] / 'golang' / 'move-to-front' / 'experiments.txt'
@@ -9,37 +9,52 @@ assert data_path.exists() and data_path.is_file()
 with open(data_path) as file:
     file_contents = file.readlines()
 
-pattern = r'LIST SIZE: (\d.+) . REQUESTS SIZE: (\d.+) .RATIO: (.+)'
+pattern = r'(DIFF|EQUAL) | LIST SIZE: (\d.+) . REQUESTS SIZE: (\d.+) .RATIO: (.+)'
 
-data = {}
+data = {
+    'DIFF': {},
+    'EQUAL': {}
+}
 for it, exp_str in enumerate(file_contents):
-    list_len, request_num, ratio = re.findall(pattern, exp_str)[0]
+    found = re.findall(pattern, exp_str)
+    operation = found[0][0]
+    _, list_len, request_num, ratio = found[1]
     list_len, request_num, ratio = int(list_len), int(request_num), float(ratio)
-    data[list_len] = ratio
+    data[operation][request_num] = ratio
 
-# Plot each list as a line
-# for key, value in data.items():
-x,y = list(data.keys()), list(data.values())
-plt.plot(x,y, color='midnightblue')
+fig, axes = plt.subplots(2, 1, figsize=(10, 8))
 
-plt.scatter(x,y, color='midnightblue')
+# First subplot (EQUAL)
+x, y = list(data['EQUAL'].keys()), list(data['EQUAL'].values())
+axes[0].plot(x, y, color='midnightblue')
+axes[0].scatter(x, y, color='midnightblue', label='Pior Caso')
+axes[0].axhline(y=4, color='black', linestyle='--', label='Competitividade')
+# axes[0].set_xlabel('Tamanho de Lista/Requisições')
+axes[0].set_ylabel('Razão entre Operações Realizadas')
+axes[0].legend()
+axes[0].set_title('Razão de Operações por Buscas')
+axes[0].grid(True)
 
-# Add horizontal line at y=4
-plt.axhline(y=4, color='black', linestyle='--', label='Competitividade')
+# Second subplot (DIFF)
+x, y = list(data['DIFF'].keys()), list(data['DIFF'].values())
+axes[1].plot(x, y, color='midnightblue')
+axes[1].scatter(x, y, color='midnightblue', label="Aleatório")
+axes[1].set_xlabel('Número de Requisições')
+axes[1].set_ylabel('Razão entre Operações Realizadas')
+axes[1].legend()
+axes[1].grid(True)
 
-# Set x and y axis labels
-plt.xlabel('Tamanho de Lista/Requisições')
-plt.ylabel('Razão entre Operações Realizadas')
+def tick_function(x):
+    v = x/900
+    return ["%.1f" % z for z in v]
 
-# Add legend and title
-plt.legend()
-plt.title('Razão de Operações por Buscas')
-
-# Grid
-plt.grid(True)
+ax2 = axes[1].twiny()
+new_tick_locations = np.array([18e3,36e3, 54e3, 72e3, 90e3])
+ax2.set_xticks(new_tick_locations)
+ax2.set_xticklabels(tick_function(new_tick_locations))
 
 # Save the figure with tight layout and high resolution
-plt.savefig(data_path.parent / 'ratio.png', dpi=1000, bbox_inches='tight')
+plt.savefig(data_path.parent / 'experiments.png', dpi=1000, bbox_inches='tight')
 
-# Show the plot
-plt.show()
+# # Show the plot
+# plt.show()
