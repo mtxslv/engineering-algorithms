2f50d485dbaf821b6daf0198d7ec8f2a483d6ae8
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-12-15 17:41:25 -0300

    chore: remove old comments

--- golang/least-recently-used/main.go
+++ golang/least-recently-used/main.go
@@ -5,27 +5,3 @@ import "utils/utils"
 func main() {
 	utils.SimulationWithRandomness()
 }
-
-// package main
-
-// import "fmt"
-
-// func main(){
-// 	var cacheHashTable = map[int]string{}
-// 	cacheHashTable[1] = "baguga"
-// 	cacheHashTable[2] = "glub glub"
-// 	cacheHashTable[3] = "mamada"
-// 	cacheHashTable[4] = ""
-	
-// 	for k := range cacheHashTable{
-// 		fmt.Printf("%s\n",cacheHashTable[k])
-// 	}
-
-// 	delete(cacheHashTable,2)
-
-// 	fmt.Printf("\n\nDELETED MIDDLE ITEM\n\n")
-
-// 	for k := range cacheHashTable{
-// 		fmt.Printf("%s\n",cacheHashTable[k])
-// 	}
-// }
