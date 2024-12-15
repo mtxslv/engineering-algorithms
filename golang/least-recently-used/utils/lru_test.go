e9c98bc84f67e49125e723afaf61d5fcff17cbf6
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-12-15 16:40:55 -0300

    test: lru get

--- golang/least-recently-used/utils/lru_test.go
+++ golang/least-recently-used/utils/lru_test.go
@@ -9,6 +9,17 @@ func initializeEmptyCache() *LRUCache{
 	return NewLRUCache(cacheSize)
 }
 
+func initializeFullCache() *LRUCache{
+	cacheSize := 3
+	lru := NewLRUCache(cacheSize)
+	
+	lru.Put(1,10)
+	lru.Put(2,20)
+	lru.Put(3,30)
+
+	return lru
+}
+
 func TestPut(t *testing.T){
 	lruCache := initializeEmptyCache()
 	lruCache.Put(5,50)
@@ -25,4 +36,26 @@ func TestPut(t *testing.T){
 		el := lruCache.cache[k]
 		t.Logf("AN ITEM: %+v\n", el.Value)
 	}
+}
+
+func TestGet(t *testing.T){
+	lru := initializeFullCache()
+	current := lru.list.Front()
+	// items are:
+	// {3:30}->{2:20}->{1:10}
+	for current != nil {
+		t.Logf("LIST ITEM: %+v", current.Value)
+		current = current.Next()
+	}
+
+	t.Logf("Search for 1\n")
+	lru.Get(1)
+	
+	// Start from the beginning
+	current = lru.list.Front()
+	for current != nil {
+		t.Logf("LIST ITEM: %+v", current.Value)
+		current = current.Next()
+	}
+	
 }
