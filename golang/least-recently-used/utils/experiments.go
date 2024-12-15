bcbc2389acebc4a8f6c8b4b28842a9f7ad694afc
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-12-15 15:52:18 -0300

    feat: new experiment (input size increases)

--- golang/least-recently-used/utils/experiments.go
+++ golang/least-recently-used/utils/experiments.go
@@ -15,3 +15,24 @@ func SimulateRequests(cacheSize int, requests []int) {
 
 	fmt.Printf("Cache Size: %d, Requests: %d, LRU Misses: %d\n", cacheSize, len(requests), lruMisses)
 }
+
+func SimulateRequestsVaryingInputSize(cacheSize int, inputSizes []int) {
+	for _, size := range inputSizes {
+		requests := make([]int, size)
+		for i := 0; i < size; i++ {
+			requests[i] = i % (cacheSize + 1)
+		}
+
+		lru := NewLRUCache(cacheSize)
+		lruMisses := 0
+
+		for _, req := range requests {
+			if !lru.Get(req) {
+				lruMisses++
+				lru.Put(req, req)
+			}
+		}
+
+		fmt.Printf("Cache Size: %d, Input Size: %d, LRU Misses: %d\n", cacheSize, size, lruMisses)
+	}
+}
