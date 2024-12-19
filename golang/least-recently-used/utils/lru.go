9176ec43dd887f6e1032e9542afbf5e85302e65d
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-12-19 17:34:03 -0300

    feat: OPT Cache first iteration

--- golang/least-recently-used/utils/lru.go
+++ golang/least-recently-used/utils/lru.go
@@ -124,3 +124,108 @@ func (c *LRUCacheV1) Get(key string) (float32, bool) {
 		return value, true
 	}
 }
+
+///// OFFLINE ALGORITHM (BELADY) //////
+/*
+LINKS:
+	https://www.cs.cornell.edu/courses/cs4410/2015su/lectures/lec15-replacement.html
+	https://www2.cs.uregina.ca/~hamilton/courses/330/notes/memory/page_replacement.html
+*/
+
+type LRUCacheOPT struct {
+	capacity int 
+	cache map[string]*list.Element
+	list *list.List
+	requests []string
+	lastRequestNum int
+}
+
+func NewOPTCache(capacity int, requests []string) *LRUCacheOPT {
+	return &LRUCacheOPT{
+		capacity: capacity,
+		cache: make(map[string]*list.Element),
+		list: list.New(),
+		requests: requests,
+		lastRequestNum: -1, // no request made		
+	}
+}
+
+func (c *LRUCacheOPT) Get(key string) (float32, bool) {
+	// New request just made. Update counter!
+	c.lastRequestNum++
+	// Does the element exist?
+	el, found := c.cache[key]	
+	if !found{
+		// No, return nothing
+		return 0.0,false
+	} else {
+		// Since it is recently used
+		// Move to front
+		c.list.MoveToFront(el)
+		value := el.Value.(entryV1).value
+		return value, true
+	}
+}
+
+func (c *LRUCacheOPT) CheckLateUse() *list.Element {
+	
+	// Variables
+	elToEvict := c.list.Front()
+	furthest := -1
+	key := ""
+	var diff int
+	
+	// Elements to iterate
+	it := c.lastRequestNum + 1
+	current := c.list.Front()
+	
+	for current != nil {
+		// This cache element's key
+		key = current.Value.(entryV1).key
+
+		for it < len(c.requests){
+			// If key appear ....
+			if c.requests[it] == key {
+				diff = it - c.lastRequestNum 
+				// ... furthest in future ...
+				if diff > furthest {
+					// Update the element to be
+					//  evicted and the furthest seen
+					elToEvict = current
+					furthest = diff
+				}
+			}
+			it++
+		}
+		current = current.Next()
+	}
+	return elToEvict
+}
+
+func (c *LRUCacheOPT) Put(key string, value float32) {
+	// Does the element exist?
+	el, found := c.cache[key]
+	if !found { // Does not
+		// Check if cache is full
+		if c.list.Len() == c.capacity { // It is. 
+			// Who to evict? The item to be 
+			// used farthest into the future.
+			// The current request is c.lastRequestNum
+			// We need to check which element from cache
+			// will be used the farthest into future from
+			// c.lastRequestNum on
+			elToEvict := c.CheckLateUse()
+			c.list.Remove(elToEvict)
+			delete(c.cache, elToEvict.Value.(entryV1).key)
+		}
+		// Add to front list (recently used)
+		el = c.list.PushFront(entryV1{key,value})
+		// Save on hash
+		c.cache[key] = el		
+	} else {
+		// Element does exist, move to front
+		c.list.MoveToFront(el)
+		el.Value = entryV1{key, value}
+	}
+	// and update the value
+}
