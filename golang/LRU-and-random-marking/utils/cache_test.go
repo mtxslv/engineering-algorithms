f01c40ba5f178eb62b6eec559b4c2a27943f5c08
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-12-20 18:22:17 -0300

    test: new marking cache

---
+++ golang/LRU-and-random-marking/utils/cache_test.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"testing"
+	"errors"
+)
+
+func TestBitwiseOperation(t *testing.T) {
+	x := uint16(0) /* 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 */
+	x = x + 1 << 5 /* 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 */
+	if x != uint16(32){
+		t.Fail()
+	}
+	i := 2
+	x = x + 1 << i /* 0 0 0 0 0 0 0 0 0 0 0 1 0 1 0 0 */ 
+	t.Logf("%d", x)
+	if x != uint16(36) {
+		t.Fail()
+	}
+}
+
+func TestMakeMarkingCache(t *testing.T) {
+	_, err := NewRandomMarkingCache(20) // 20 > 16
+	if !errors.Is(err, ErrCacheSizeTooLarge) {
+		t.Fail()
+	}
+
+	cache, err := NewRandomMarkingCache(15)
+	if err != nil || cache.capacity != 15 {
+		t.Fail()
+	}
+}
