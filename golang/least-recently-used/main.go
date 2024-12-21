e2487331bf03b5f7ca1fc8f7672b97503bfabfe4
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-12-21 11:26:11 -0300

    fix: expand universe of values

--- golang/least-recently-used/main.go
+++ golang/least-recently-used/main.go
@@ -13,11 +13,13 @@ import (
 )
 
 func main(){
+	universeValues := 5000
+	codeSize := 10
 	N := 500
 	for N < 1000 {
 		K := 3 
 		for K < 15 {
-			utils.SimulationLRUOPT(6,10,N,K)
+			utils.SimulationLRUOPT(codeSize,universeValues,N,K)
 			K++
 		}
 		N++
