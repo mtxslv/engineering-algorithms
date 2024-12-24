4b97fe56115dfef7a842f468e90756dc465c344e
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-12-24 15:36:11 -0300

    feat: add lup on main

--- cpp/linear-solver/main.cpp
+++ cpp/linear-solver/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <vector>
 #include "file1.h"
+#include "lup.h"
+
+using namespace std;
 
 void demo(){
     std::cout << "This is a demo" << std::endl;
@@ -9,7 +13,23 @@ void demo(){
     std::cout << std::endl;
 }
 
+
+void testingLUPsolver(){
+    vector<vector<float>> L;
+    vector<vector<float>> U;
+    // Create and initialize the 2D vector (matrix)
+    vector<vector<float>> matrix = {
+        {1.0f, 0.0f, 0.0f},
+        {0.2f, 2.0f, 0.0f},
+        {0.6f, 0.5f, 1.0f}
+    };
+
+    // Print the matrix
+    printMatrix(matrix);
+}
+
 int main(){
     demo();
+    testingLUPsolver();
     return 0;
 }
