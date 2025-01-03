3386f22250b85e28ef2b9aa235d6f22d3b2d2385
Mateus Assis <mateus.d.assis.silva@gmail.com>
2025-01-03 10:52:56 -0300

    feat: call LupInverse

--- cpp/linear-solver/main.cpp
+++ cpp/linear-solver/main.cpp
@@ -12,15 +12,14 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    vector<vector<float>> T = readFile(argv[1]); // Tableau matrix
+    vector<vector<float>> A = readFile(argv[1]); // Tableau matrix
 
-    if (!checkMatrixSquare(T)) {
+    if (!checkMatrixSquare(A)) {
         cout << "Matrix must be square" << endl;
         return 1;
     }
 
-    // simplexTableau(T,n,m);
-    // cout << "Processed tableau containing "<< n << " variable(s) and " << m << " constraint(s)." << endl;
-    // string ans = processOutput(T,n,m);
-    // cout << ans;
+    vector<vector<float>> Inverse = LupInverse(A);
+
+    printMatrix(Inverse);
 }
