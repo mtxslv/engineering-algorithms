10e3c4796fcf466ebed8c504c8271df1f448e3ef
Mateus Assis <mateus.d.assis.silva@gmail.com>
2025-01-17 18:38:36 -0300

    feat: experiment running ln(1+x)

--- cpp/fft-and-polynomials/main.cpp
+++ cpp/fft-and-polynomials/main.cpp
@@ -1,8 +1,24 @@
+#include <iomanip>
 #include <iostream>
 #include <cstring>
+#include <vector>
 #include "fft.h"
+#include "polynomial.h"
 #include "taylorSeries.h"
 
 using namespace std;
 
-int main(int argc, char* argv[]) {}
+int main(int argc, char* argv[]) {
+    vector<double> params;
+    double x = 0.5;
+    double y ;
+    cout << "NATURAL LOGARITHM OF " << 1 + x << endl;
+    
+    int terms = 100+1 ;
+    for(int k = 10; k <= terms; k++){
+        params = naturalLogarithm(k);
+        y = poly(x,params);
+        cout << "\t" << k <<" params: " ;
+        cout << setprecision(25) << y << endl;
+    }   
+}
