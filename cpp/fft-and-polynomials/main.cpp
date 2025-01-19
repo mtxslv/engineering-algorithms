e295219309f35a6d11bc280755723bb590699e01
Mateus Assis <mateus.d.assis.silva@gmail.com>
2025-01-19 12:13:20 -0300

    chore: further testing dft

--- cpp/fft-and-polynomials/main.cpp
+++ cpp/fft-and-polynomials/main.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    vector<double> a = {1.0, 2.0, 3.0, 4.0};
+    vector<double> a = {1.0, 2.0, 3.0, 4.0, 5.0};
     vector<complex<double>> dftA = DFT(a);
     for (int k = 0; k < 4; k++) {
         cout << dftA[k] << endl;
--- python/fft-poly/main.py
+++ python/fft-poly/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-A = np.array([1.0, 2.0, 3.0, 4.0])
+A = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
 n = len(A)
 N = 1 << (n - 1).bit_length()  # Next power of two
 # A_padded = np.pad(A, (0, N - len(A)))
