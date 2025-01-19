6e57bb27b350bf413c02b6abffded699ebca57b2
Mateus Assis <mateus.d.assis.silva@gmail.com>
2025-01-19 11:34:26 -0300

    feat: return mock y in DFT & FFT

--- cpp/fft-and-polynomials/fft.cpp
+++ cpp/fft-and-polynomials/fft.cpp
@@ -5,6 +5,24 @@ using namespace std;
 
 vector<complex<double>> DFT(vector<double> a){
     int n = a.size();
-    // vector<complex<double> y = vector<complex>(n,complex(0,0));
+    vector<complex<double>> y = vector<complex<double>>(n,complex<double>(0,0));
+    
+    complex<double> unity_root_k;
+    for (int k = 0; k < n ; k++) {
+        unity_root_k = polar(1.0,(double)n/(double)k);
+        for (int j = 0; j < n; j++) {
+            // y[k] += a[j]
+        }
+    }
+    return y;
 }
-vector<complex<double>> FFT(vector<double> a){}
+
+vector<complex<double>> FFT(vector<double> a){
+    int n = a.size();
+    vector<complex<double>> y = vector<complex<double>>(n,complex<double>(0,0));
+    return y;
+}
+
+
+//https://www.reddit.com/r/DSP/comments/13ji73d/question_about_c_implementation_of_dft/
+// https://stackoverflow.com/questions/51679516/discrete-fourier-transform-c
