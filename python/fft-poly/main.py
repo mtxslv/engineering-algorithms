1bb43cff5d8bc80eab19f10749e7fb426224055f
Mateus Assis <mateus.d.assis.silva@gmail.com>
2025-01-19 16:58:02 -0300

    chore: more testes with fft and padding

--- python/fft-poly/main.py
+++ python/fft-poly/main.py
@@ -1,8 +1,9 @@
 import numpy as np
 
-A = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
+# A = np.array([1.0, 2.0])
+A = np.array([1.0, 2.0, 3.0,4.0,5.0])
 n = len(A)
 N = 1 << (n - 1).bit_length()  # Next power of two
-# A_padded = np.pad(A, (0, N - len(A)))
-A_fft = np.fft.fft(A)
+A_padded = np.pad(A, (0, N - len(A)))
+A_fft = np.fft.fft(A_padded)
 print(A_fft)
