1e5edb04aaac9f381a81f4cd52cea6ec5fc695a9
Mateus Assis <mateus.d.assis.silva@gmail.com>
2025-01-20 14:39:12 -0300

    fix: conv1D

--- cpp/fft-and-polynomials/polynomial.cpp
+++ cpp/fft-and-polynomials/polynomial.cpp
@@ -2,7 +2,7 @@
 #include <math.h>       /* pow */
 #include <complex>
 #include <vector>
-#include <iostream>
+#include <iostream>     /* APAGAR DEPOIS */
 
 using namespace std;
 
@@ -30,13 +30,18 @@ std::vector<std::complex<double>> conv1D(
     std::vector<std::complex<double>> result(outSize);
 
     // Perform the convolution
-    for (int i = 0; i < outSize; ++i) {
+    int indexA, indexB;
+    for (int k = 0; k < outSize; k++) {
         std::complex<double> sum(0.0, 0.0);
-        for (int j = 0; j < m; ++j) {
-            sum += A[i + j]*B[m - j - 1]; // Note the reversal of B
+        for (int i = 0 ; i < n; i++) {
+            indexA = i ;
+            indexB = k-i; // Note the reversal of B
+            if (indexA >= 0 && indexA < n && indexB >= 0 && indexB < m) {
+                // If the index are valid (inside the limits) we consider
+                sum += A[indexA]*B[indexB]; 
+            } 
         }
-        result[i] = sum;
+        result[k] = sum;
     }
-
     return result;
 }
